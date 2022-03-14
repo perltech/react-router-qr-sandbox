6b2e5a4a930a591f5ab0385109d3b898bca0b3c4
perltech <noah.harrison@protonmail.com>
2022-03-14 12:42:46 -0600

    adds conditional rendering for qr code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { useState } from 'react';
+
 import { Link, Outlet } from "react-router-dom";
 
 var QRCode = require('qrcode.react');
 
 function App() {
+  const [phrase, setPhrase] = useState("");
+  let secretPhrase = "Happy";
+  let hasQRCode = false;
+
+  // const handleInput = () => {
+  //   if (phrase == secretPhrase) {
+  //     return hasQRCode = true;
+  //   }
+  // }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -14,10 +26,15 @@ function App() {
         <h2>
           Happy
           <br />
+          <input placeholder={"What is the secret phrase?"} onChange={e => setPhrase(e.target.value)}/>
+          {/* <button onClick={() => handleInput()}>Submit</button> */}
           <br />
           {/* Displays ONLY the QR code without creating an active link, that connects to the route from index.js */}
             {/* <Link to="/happy"> */}
+            {/* if (hasQRCode) {
               <QRCode value="http://localhost:3001/happy"/>
+            } */}
+            {phrase === secretPhrase ? <QRCode value="http://localhost:3001/happy"/> : ""}
             {/* </Link> */}
         </h2>
         <h2>
